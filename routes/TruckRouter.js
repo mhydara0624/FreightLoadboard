3c5b24b1b2751a117c59d73655741dbc1d95f001
mhydara0624 <mhydara0624@icloud.com>
2021-10-12 15:06:42 -0400

    added truck router

---
+++ routes/TruckRouter.js
@@ -0,0 +1,26 @@
+const controller = require('../controllers/TruckController')
+const middleware = require('../middleware')
+const router = require('express').Router()
+
+router.get('/', controller.GetTrucks)
+
+router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateTruck
+)
+router.put(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateTruck
+)
+router.delete(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteTruck
+)
+
+module.exports = router
