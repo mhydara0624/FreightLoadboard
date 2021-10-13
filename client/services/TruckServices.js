8d27623d2108690be18f3495da50624a69de9fc2
mhydara0624 <mhydara0624@icloud.com>
2021-10-13 10:29:42 -0400

    add truck services

---
+++ client/services/TruckServices.js
@@ -0,0 +1,26 @@
+export const PostNewTruck = async (data) => {
+  try {
+    const res = await Client.post('/trucks', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdateTruck = async (id, data) => {
+  try {
+    const res = await Client.put(`/trucks/${id}`, data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeleteTruck = async (id) => {
+  try {
+    const res = await Client.delete(`/trucks/${id}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
