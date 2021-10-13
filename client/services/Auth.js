f988437afc9b10aff3fdb3b112522dad8669f524
mhydara0624 <mhydara0624@icloud.com>
2021-10-13 10:22:47 -0400

    added auth frontend service

---
+++ client/services/Auth.js
@@ -0,0 +1,29 @@
+import Client from './api'
+
+export const SignInUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/login', data)
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CheckSession = async () => {
+  try {
+    const res = await Client.get('/auth/session')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
