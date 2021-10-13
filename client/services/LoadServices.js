d13ca2dd6b9df2906cb3fe779bd8c70f1d8244e3
mhydara0624 <mhydara0624@icloud.com>
2021-10-13 11:43:33 -0400

    added load service/edit user service

---
+++ client/services/LoadServices.js
@@ -0,0 +1,26 @@
+export const PostNewLoad = async (data) => {
+  try {
+    const res = await Client.post('/loads', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdateLoad = async (id, data) => {
+  try {
+    const res = await Client.put(`/loads/${id}`, data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeleteLoad = async (id) => {
+  try {
+    const res = await Client.delete(`/loads/${id}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
